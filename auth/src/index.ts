ebfef864eb2060a979283857ad732447b156da2f
petefletcher <petefletch81@gmail.com>
2020-10-01 18:36:40 +0100

    feat: add ingress-nginx service [PF]

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentUser", (req, res) => {
+  res.send("hi there!");
+});
+
 app.listen(3000, () => {
-  console.log("Listening on 3000");
+  console.log("Listening on 3000!!");
 });
