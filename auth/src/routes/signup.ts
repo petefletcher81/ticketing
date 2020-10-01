1af92d5b38b165fcd0f116ecf2330d4d35304630
petefletcher <petefletch81@gmail.com>
2020-10-01 19:34:16 +0100

    feat(signup): add validation and error handling phase 1 [PF]

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,33 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 // create a router to set up routes
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Woo haa");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    // look as req object
+    const errors = validationResult(req);
+
+    // check for errors
+    if (!errors.isEmpty()) {
+      // errors array - safely sends data back to user as an array within the json
+      throw new Error("Invalid email or password");
+    }
+
+    const { email, password } = req.body;
+    console.log("Creating a user");
+    throw new Error("Error connecting to databse");
+    res.send("Yeah yup!");
+  }
+);
 
 // export and rename --> going to have many routers so need to rename each
 export { router as signupRouter };
