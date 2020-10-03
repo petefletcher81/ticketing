62a49962d06ab286e1494d389e5fe52954aa425a
petefletcher <petefletch81@gmail.com>
2020-10-03 18:45:35 +0100

    feat: add mongoose for mongo connection [PF]

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
-
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -26,6 +26,22 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on 3000!!");
-});
+const start = async () => {
+  // within the mongo depl -- find the service name to connect to
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to mongo db");
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on 3000!!");
+  });
+};
+
+start();
